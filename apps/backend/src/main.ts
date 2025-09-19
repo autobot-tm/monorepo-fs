a09b5fec62c60bc570c8d195cc1300b6db1f15de
thanhnm <thanhnm@vmsco.vn>
2025-09-19 08:39:18 +0700

    chore: get .env

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import dotenv from 'dotenv';
+import path from 'path';
 import { AppModule } from './app.module';
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+});
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    console.log(`Listening on port ${process.env.PORT}`);
+
     await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+
+bootstrap().catch(err => {
+    console.error('Bootstrap error:', err);
+    process.exit(1);
+});
